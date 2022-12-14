76722531fe732a75003bdd9de065f32af124fb12
Yiğit Cumhur Erkan <erkan.cumhur@hotmail.com>
2022-12-14 20:56:36 +0300

    Initial commit

---
+++ pages/admin/new-post.vue
@@ -0,0 +1,25 @@
+<template>
+  <PostForm @submit="savePost($event)" />
+</template>
+<script>
+import PostForm from "@/components/admin/PostForm";
+import axios from "axios";
+export default {
+  components: {
+    PostForm,
+  },
+  methods: {
+    savePost(post) {
+      axios
+        .post(
+          "https://newspaper-column-default-rtdb.firebaseio.com/posts.json",
+          post,
+        )
+        .then((res) => {
+          console.log(res);
+          this.$router.push("/admin");
+        });
+    },
+  },
+};
+</script>
