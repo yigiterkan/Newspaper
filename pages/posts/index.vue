d0b6461ce3d6b3d87ce2657d7fa7c062d1613730
Yiğit Cumhur Erkan <erkan.cumhur@hotmail.com>
2022-12-18 17:06:44 +0300

    axios added

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -10,27 +10,10 @@ export default {
   components: {
     PostList,
   },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        fetchedPosts: [
-          {
-            id: 1,
-            title: "NuxtJS",
-            subTitle: "Ilk NuxtJS projesi",
-            text: "Bu proje nuxtJs bootstrap vs kullanilarak yapilmistir:)",
-            author: "Yigit Erkan",
-          },
-          {
-            id: 2,
-            title: "NextJS",
-            subTitle: "Ilk NextJS projesi için içerik",
-            text: "Bir sonraki projeyi NextJS ile yapmayı dusunuyorum :)",
-            author: "Yigit Cumhur Erkan",
-          },
-        ],
-      });
-    }, 2000);
+  computed: {
+    fetchedPosts() {
+      return this.$store.getters.getPosts;
+    },
   },
 };
 </script>
--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from "vuex";
+import axios from "axios";
 
 const createStore = () => {
   return new Vuex.Store({
@@ -12,23 +13,20 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
-        vuexContext.commit("setPosts", [
-          {
-            id: 1,
-            title: "Donec ac odio ",
-            subTitle: "Tempor Orci. ",
-            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
-            author: "Sem Fringilla",
-          },
-          {
-            id: 2,
-            title: "Cras",
-            subTitle: "Pulvinar",
-            text: "Temporibus quaerat adipisci praesentium, voluptas perspiciatis voluptatem nam alias aliquam ipsa sit ducimus tempore sapiente possimus distinctio quae molestiae et sequi fugit!",
-            author: "Ultricies Leo",
-          },
-        ]);
+        return axios
+          .get(
+            "https://newspaper-column-default-rtdb.firebaseio.com/posts.json",
+          )
+          .then((response) => {
+            let data = response.data;
+            let postArray = [];
+            for (let key in data) {
+              postArray.push({ id: key, ...data[key] });
+            }
+            vuexContext.commit("setPosts", postArray);
+          });
       },
+
       setPosts(vuexContext, posts) {
         vuexContext.commit("setPosts", posts);
       },
